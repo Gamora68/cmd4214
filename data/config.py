2de5cc2f35f487f9b24c66ddcdcace3ca01b7d06
Gamora68 <120760467+Gamora68@users.noreply.github.com>
2023-03-29 16:37:58 +0200

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,7 @@
+# Config
+token = '5884128964:AAFgSEp9SL8sTfJCedfAZgajxkTddgyXwQA'
+photo = 'https://gfycat.com/boldoilyiriomotecat' # imgur.com
+admins = [1172548858]
+
+cryptopay_token = '85320:AAtp7PNThpwx3NVG6Dtwc4aoq2JpqFgNvTZ' # CryptoBot
+logs = '-100' # Channel Logs
